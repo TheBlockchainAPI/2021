e4758fc901682fcfa6b656f037eb1217dcc15b42
BlockchainAPI <80053050+TheBlockchainAPI@users.noreply.github.com>
2021-03-04 15:56:27 +0000

    Create generateAddress.php
    In this file, you'll want to generate an address so your customers can pay you.
    The code above sends a request to our API endpoint, generating a new address from the input address you want payments to be forwarded to and the callback url we will send your requests to.
    Always make sure you use a unique callback url... even if it's just adding a secret GET parameter!

---
+++ Python/generateAddress.php
@@ -0,0 +1,27 @@
+# Importing the requests library 
+import requests 
+  
+# Api-endpoint 
+URL = "https://aiocrypt.com/api/v1?method=create"
+  
+# Parameters 
+myAddress = "1LisLsZd3bx8U1NYzpNHqpo8Q6UCXKMJ4z"
+mySecret = "7j0ap91o99cxj8k9"
+myCallback = "http://example.com/callback?invoice_id=1234&secret=" + mySecret
+  
+# Defining a params dict for the parameters to be sent to the API 
+PARAMS = {'address':myAddress, 'callback':myCallback} 
+  
+# Sending get request and saving the response as response object 
+r = requests.get(url = URL, params = PARAMS) 
+  
+# Extracting data in json format 
+data = r.json() 
+  
+# Extracting input_address & estimated_transaction_fee
+input_address = data['success']['input_address']
+estimatedFee = data['success']['estimated_transaction_fee']
+  
+# Printing the output 
+print("Please pay to the following address: %s\nThe estimated fee in BTC is: %s"
+      %(input_address, estimatedFee)) 
