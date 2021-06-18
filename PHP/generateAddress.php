5c6f03c300ecc9a3dfe86b838512d55d8928190c
BlockchainAPI <80053050+TheBlockchainAPI@users.noreply.github.com>
2021-06-18 17:17:47 +0100

    Update generateAddress.php

--- PHP/generateAddress.php
+++ PHP/generateAddress.php
@@ -1,24 +1,21 @@
 <?php
   # PHP Example To Generate Address
-  # Change the two values below
   $secret = "7j0ap91o99cxj8k9";
   $my_address = "1LisLsZd3bx8U1NYzpNHqpo8Q6UCXKMJ4z";
-  $setting = 'catch_instant'; // catch_instant (catches payments coming into the node) | catch_forwarded (catches payments sent from our node to your destination_address)
-
+  $addressType = "bech32"; // bech32 | legacy | p2sh-segwit
   $my_callback_url = "http://example.com/callback?invoice_id=1234&secret=" . $secret;
-  $api_base = "https://blockchainapi.org/api/btc"; # Our BTC API endpoint
+  $api_base = "https://blockchainapi.org/api/btc";
 
   $curl = curl_init();
   curl_setopt_array($curl, array(
       CURLOPT_RETURNTRANSFER => 1,
-      CURLOPT_URL => $api_base . "?method=create&address=" . $my_address . "setting=".$setting."&callback=" . $my_callback_url
+      CURLOPT_URL => $api_base . "?method=create&address_type=".$addressType."address=" . $my_address . "&callback=" . $my_callback_url
   ));
 
   $response = curl_exec($curl);
   $http_status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
   curl_close($curl);
 
-  # If we get a good, 200 http status code, get the payment details: input_address & estimated_transaction_fee
   if ($http_status_code == 200) {
       $decoded = json_decode($response, true);
       echo "Please send the payment to the following Bitcoin address: " . $decoded["success"]["input_address"];
